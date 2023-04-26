0dec996f9cdc649b7ff1625594176c09f34fbf7d
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-04-26 16:48:33 +0600

    add: terms&condition page added with design

---
+++ client/src/pages/termsAndCondition/TermsAndCond.jsx
@@ -0,0 +1,310 @@
+import React from "react";
+import polygon from "../../assets/images/Polygon 1.svg";
+const TermsAndCond = ({ onClose }) => {
+  return (
+    <div
+      style={{
+        position: "relative",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: "#272c32",
+        textAlign: "left",
+        fontSize: "14px",
+        color: "#fff",
+        fontFamily: "Poppins",
+
+        padding: "20px",
+        boxSizing: "border-box",
+        // default styles above
+
+        // responsive styles below
+        maxWidth: "100%",
+
+        width: "100%",
+        height: "135vh",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          background:
+            "linear-gradient(94.24deg, #098ea1, rgba(92, 216, 235, 0.8) 94.06%)",
+          width: "100%",
+          height: "36%",
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: "270px",
+          left: "171px",
+          backgroundColor: "#171a20",
+          boxShadow: "6px 6px 9px rgba(0, 0, 0, 0.24)",
+          width: "75%",
+          height: "95vh",
+        }}
+      />
+      <img
+        style={{
+          position: "absolute",
+          top: "324.66px",
+          left: "277.66px",
+          borderRadius: "4px",
+          width: "42px",
+          height: "42px",
+        }}
+        alt=""
+        src={polygon}
+      />
+      <img
+        style={{
+          position: "absolute",
+          top: "480.66px",
+          left: "274.66px",
+          borderRadius: "4px",
+          width: "42px",
+          height: "42px",
+        }}
+        alt=""
+        src={polygon}
+      />
+      <img
+        style={{
+          position: "absolute",
+          top: "639.66px",
+          left: "274.66px",
+          borderRadius: "4px",
+          width: "42px",
+          height: "42px",
+        }}
+        alt=""
+        src={polygon}
+      />
+      <img
+        style={{
+          position: "absolute",
+          top: "841.66px",
+          left: "277.66px",
+          borderRadius: "4px",
+          width: "42px",
+          height: "42px",
+        }}
+        alt=""
+        src={polygon}
+      />
+      <b
+        style={{
+          position: "absolute",
+          top: "334px",
+          left: "294px",
+          textShadow: "0px 0px 0px rgba(255, 255, 255, 0.01)",
+        }}
+      >
+        1
+      </b>
+      <b
+        style={{
+          position: "absolute",
+          top: "490px",
+          left: "290px",
+          textShadow: "0px 0px 0px rgba(255, 255, 255, 0.01)",
+        }}
+      >
+        2
+      </b>
+      <b
+        style={{
+          position: "absolute",
+          top: "649px",
+          left: "290px",
+          textShadow: "0px 0px 0px rgba(255, 255, 255, 0.01)",
+        }}
+      >
+        3
+      </b>
+      <b
+        style={{
+          position: "absolute",
+          top: "852px",
+          left: "291px",
+          textShadow: "0px 0px 0px rgba(255, 255, 255, 0.01)",
+        }}
+      >
+        4
+      </b>
+      <div
+        style={{
+          position: "absolute",
+          top: "10%",
+          left: "30%",
+          fontSize: "3.6vw",
+          fontWeight: "600",
+          color: "rgba(0, 0, 0, 0.85)",
+          textShadow: "0px 0px 0px rgba(255, 255, 255, 0.01)",
+        }}
+      >{`TERMS & CONDITIONS`}</div>
+      <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <button
+          style={{
+            cursor: "pointer",
+            border: "none",
+            padding: "0",
+            backgroundColor: "#000",
+            position: "absolute",
+            top: "18%",
+            left: "40%",
+            borderRadius: "50px",
+            boxShadow: "0px 0px 0px 4px rgba(255, 255, 255, 0.01)",
+            width: "202px",
+            height: "48px",
+          }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: "19.2%",
+            left: "42%",
+            whiteSpace: "nowrap",
+            display: "inline-block",
+            width: "15%",
+            height: "2%",
+            textShadow: "0px 0px 0px rgba(255, 255, 255, 0.01)",
+          }}
+        >
+          Updated Dec 21, 2022
+        </div>
+      </div>
+
+      <div
+        style={{
+          position: "absolute",
+          top: "326px",
+          left: "345px",
+          fontSize: "22px",
+          fontWeight: "600",
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "484px",
+          left: "345px",
+          fontSize: "22px",
+          fontWeight: "600",
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "843px",
+          left: "345px",
+          fontSize: "22px",
+          fontWeight: "600",
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "642px",
+          left: "345px",
+          fontSize: "22px",
+          fontWeight: "600",
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "361px",
+          left: "345px",
+          fontSize: "18px",
+          color: "#cccbcb",
+          textAlign: "justify",
+          display: "inline-block",
+          width: "50%",
+          height: "9%",
+          textShadow: "0px 0px 0px rgba(255, 255, 255, 0.01)",
+          marginBottom: "2%",
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget
+        venenatis odio. Praesent et eros eros. Etiam fringilla est sed mi
+        efficitur pellentesque. Nunc tristique elit sapien. Fusce ut odio sit
+        amet odio feugiat consequat et et est.
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "519px",
+          left: "345px",
+          fontSize: "18px",
+          color: "#cccbcb",
+          textAlign: "justify",
+          display: "inline-block",
+          width: "50%",
+          height: "9%",
+          textShadow: "0px 0px 0px rgba(255, 255, 255, 0.01)",
+          marginBottom: "2%",
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget
+        venenatis odio. Praesent et eros eros. Etiam fringilla est sed mi
+        efficitur pellentesque. Nunc tristique elit sapien. Fusce ut odio sit
+        amet odio feugiat consequat et et est.
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "677px",
+          left: "345px",
+          fontSize: "18px",
+          color: "#cccbcb",
+          textAlign: "justify",
+          display: "inline-block",
+          width: "50%",
+          height: "9%",
+          textShadow: "0px 0px 0px rgba(255, 255, 255, 0.01)",
+          marginBottom: "2%",
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget
+        venenatis odio. Praesent et eros eros. Etiam fringilla est sed mi
+        efficitur pellentesque. Nunc tristique elit sapien. Fusce ut odio sit
+        amet odio feugiat consequat et et est.Duis vitae blandit lectus.
+        Vestibulum id ullamcorper augue. Cras non tincidunt quam, vel fringilla
+        elit. Ut porttitor sit amet mauris nec faucibus. Donec elementum
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "876px",
+          left: "341px",
+          fontSize: "18px",
+          color: "#cccbcb",
+          textAlign: "justify",
+          display: "inline-block",
+          width: "50%",
+          height: "9%",
+          marginBottom: "2%",
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget
+        venenatis odio. Praes . Fusce ut odio sit amet odio feugiat consequat et
+        et est.Duis vitae blandit
+      </div>
+    </div>
+  );
+};
+
+export default TermsAndCond;
