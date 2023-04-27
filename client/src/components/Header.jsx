139cc620d56d333f17835d0fa3354b1201cd7f89
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-04-27 11:58:25 +0600

    add:Header updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Header.css"
 const Header = ({ category, title }) => (
   <div className="container py-5 px-5 mx-0 min-w-full flex flex-col items-center">
-    <p className="text-3xl text-sky-300 title-border mb-2 p-2">{category}</p>
+    <p className="text-4xl text-[#69CFE6] title-border mb-2 p-2">{category}</p>
 
     <p className="text-1xl tracking-tight text-sky-300">{title}</p>
   </div>
