e5c09c43380258ffb8eaf44ab1bdb90fdcdb7924
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-05-16 00:00:21 +0600

    mdified:apiSlice.jsx Updated

--- client/src/services/apiSlice.js
+++ client/src/services/apiSlice.js
@@ -1,149 +1,161 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-	reducerPath: "api",
-	baseQuery: fetchBaseQuery({
-		baseUrl: "http://localhost:8080/",
-		credentials: "include",
-	}),
-	tagTypes: [
-		"isLogin",
-		"profile",
-		"currentUser",
-		"isTeacher",
-		"class",
-		"topic",
-		"course",
-		"content",
-	],
-	endpoints: (builder) => ({
-		// auth api start
-		register: builder.mutation({
-			query: (signupData) => ({
-				url: "api/user/register",
-				method: "POST",
-				body: signupData,
-				headers: {
-					"Content-type": "application/json; charset=UTF-8",
-				},
-			}),
-		}),
-		login: builder.mutation({
-			query: (loginData) => ({
-				url: "api/user/login",
-				method: "POST",
-				body: loginData,
-				headers: {
-					"Content-type": "application/json; charset=UTF-8",
-				},
-			}),
-			invalidatesTags: ["isLogin", "profile", "currentUser", "isTeacher"],
-		}),
-		isLoggedIn: builder.query({
-			query: () => ({
-				url: "api/user/isLoggedIn",
-				method: "GET",
-			}),
-			providesTags: ["isLogin"],
-		}),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8080/",
+    credentials: "include",
+  }),
+  tagTypes: [
+    "tips",
+    "isLogin",
+    "profile",
+    "currentUser",
+    "isTeacher",
+    "class",
+    "topic",
+    "course",
+    "content",
+  ],
+  endpoints: (builder) => ({
+    // auth api start
+    register: builder.mutation({
+      query: (signupData) => ({
+        url: "api/user/register",
+        method: "POST",
+        body: signupData,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+    }),
+    login: builder.mutation({
+      query: (loginData) => ({
+        url: "api/user/login",
+        method: "POST",
+        body: loginData,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      invalidatesTags: ["isLogin", "profile", "currentUser", "isTeacher"],
+    }),
+    isLoggedIn: builder.query({
+      query: () => ({
+        url: "api/user/isLoggedIn",
+        method: "GET",
+      }),
+      providesTags: ["isLogin"],
+    }),
 
-		
-		isTeacher: builder.query({
-			query: () => ({
-				url: "api/user/isTeacher",
-				method: "GET",
-			}),
-			providesTags: ["isTeacher"],
-		}),
-		logout: builder.mutation({
-			query: () => ({
-				url: "api/user/logout",
-				method: "PATCH",
-			}),
-			invalidatesTags: ["isLogin", "currentUser"],
-		}),
-		currentUser: builder.query({
-			query: () => ({
-				url: "api/user",
-				method: "GET",
-			}),
-			providesTags: ["currentUser"],
-		}),
+    isTeacher: builder.query({
+      query: () => ({
+        url: "api/user/isTeacher",
+        method: "GET",
+      }),
+      providesTags: ["isTeacher"],
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "api/user/logout",
+        method: "PATCH",
+      }),
+      invalidatesTags: ["isLogin", "currentUser"],
+    }),
+    currentUser: builder.query({
+      query: () => ({
+        url: "api/user",
+        method: "GET",
+      }),
+      providesTags: ["currentUser"],
+    }),
 
-		getClasses: builder.query({
-			query: () => ({
-				url: "api/class",
-				method: "GET",
-			}),
-			providesTags: ["class"],
-		}),
-		addClass: builder.mutation({
-			query: (formData) => ({
-				url: "api/class/add",
-				method: "POST",
-				body: formData,
-			}),
-			invalidatesTags: ["class"],
-		}),
+    getClasses: builder.query({
+      query: () => ({
+        url: "api/class",
+        method: "GET",
+      }),
+      providesTags: ["class"],
+    }),
 
-		viewCourses: builder.query({
-			query: () => ({
-				url: `api/course`,
-				method: "GET",
-			}),
-			providesTags: ["course"],
-		}),
-		addCourse: builder.mutation({
-			query: (formData) => ({
-				url: `api/course/add`,
-				method: "POST",
-				body: formData,
-			}),
-			invalidatesTags: ["course"],
-		}),
+    viewCourses: builder.query({
+      query: () => ({
+        url: `api/course`,
+        method: "GET",
+      }),
+      providesTags: ["course"],
+    }),
 
-		viewTopic: builder.query({
-			query: () => ({
-				url: `api/topic`,
-				method: "GET",
-			}),
-			providesTags: ["topic"],
-		}),
-		addTopic: builder.mutation({
-			query: (formData) => ({
-				url: `api/topic/add`,
-				method: "POST",
-				body: formData,
-			}),
-			invalidatesTags: ["topic"],
-		}),
+    viewTips: builder.query({
+      query: () => ({
+        url: `api/tips`,
+        method: "GET",
+      }),
+      providesTags: ["tips"],
+    }),
 
-		viewTopicContent: builder.query({
-			query: () => ({
-				url: `api/topicContent`,
-				method: "GET",
-			}),
-			providesTags: ["content"],
-		}),
-		addTopicContent: builder.mutation({
-			query: (formData) => {
-				const payload = new FormData();
+    addCourse: builder.mutation({
+      query: (formData) => ({
+        url: `api/course/add`,
+        method: "POST",
+        body: formData,
+      }),
+      invalidatesTags: ["course"],
+    }),
 
-				payload.append("topic", formData.topic);
-				payload.append("teacherName", formData.teacherName);
-				payload.append("topicName", formData.topicName);
-				payload.append("description", formData.description);
-				payload.append("topic-video", formData.video);
+    viewTopic: builder.query({
+      query: () => ({
+        url: `api/topic`,
+        method: "GET",
+      }),
+      providesTags: ["topic"],
+    }),
+    // addTopic: builder.mutation({
+    //   query: (formData) => ({
+    //     url: `api/topic/add`,
+    //     method: "POST",
+    //     body: formData,
+    //   }),
+    //   invalidatesTags: ["topic"],
+    // }),
 
-				return {
-					url: `api/topicContent/add`,
-					method: "POST",
-					body: payload,
-				};
-			},
-			invalidatesTags: ["content"],
-		}),
-	}),
+    // viewTopicContent: builder.query({
+    //   query: () => ({
+    //     url: `api/topicContent`,
+    //     method: "GET",
+    //   }),
+    //   providesTags: ["content"],
+    // }),
+
+    // addTips: builder.mutation({
+    //   query: (formData) => ({
+    //     url: "api/tips/add",
+    //     method: "POST",
+    //     body: formData,
+    //   }),
+    //   invalidatesTags: ["tips"],
+    // }),
+
+    addTips: builder.mutation({
+
+      query: (formData) => {
+        console.log(formData);
+        const payload = new FormData();
+
+        payload.append("topicName", formData.topicName);
+        payload.append("topicDesc", formData.topicDesc);
+        payload.append("topicAvatar", formData.topicAvatar);
+        
+
+        return {
+          url: `api/tips/add`,
+          method: "POST",
+          body: payload,
+        };
+      },
+      invalidatesTags: ["tips"],
+    }),
+  }),
 });
 
 export const {
@@ -155,7 +167,8 @@ export const {
 	useIsTeacherQuery,
 
 	useGetClassesQuery,
-	useAddClassMutation,
+	useAddTipsMutation,
+	useViewTipsQuery,
 
 	useViewCoursesQuery,
 	useAddCourseMutation,
@@ -163,6 +176,5 @@ export const {
 	useAddTopicMutation,
 	useViewTopicQuery,
 
-	useAddTopicContentMutation,
-	useViewTopicContentQuery,
+	
 } = api;
