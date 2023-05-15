80b867da8f66e60cff134987fb6269109e81f71c
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-05-16 00:05:40 +0600

    modified:tipsRoute updated

--- server/routes/tipsRoute.js
+++ server/routes/tipsRoute.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const { getTips, addTips } = require("../controllers/tipsController");
 // const { addClass, getClasses } = require("../controllers/tipsController");
 const { verifyToken } = require("../middlewares/jwtMiddleware");
-
-router.get("/",  getTips);
-router.post("/add",   addTips);
+const upload = require("../middlewares/upload");
+router.get("/",   getTips);
+router.post("/add", upload.single("topicAvatar"), addTips);
 
 module.exports = router;
