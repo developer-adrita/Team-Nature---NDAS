58d83f255ac7de56516bebc416206446dd410421
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-05-30 03:08:06 +0600

    modified:ControlsRow.jsx

--- client/src/pages/controls/ControlsRow.jsx
+++ client/src/pages/controls/ControlsRow.jsx
@@ -1,34 +1,45 @@
+import { toast } from "react-toastify";
+import { useDeleteTipMutation } from "../../services/apiSlice";
+
 const ControlsRow = ({ control }) => {
+  const [deleteControl, deleteInfo] = useDeleteTipMutation();
+
+  const handleDelete = () => {
+    deleteControl(control._id)
+      .unwrap()
+      .then((response) => {
+        if (response.success) {
+          toast.success(`Tip deleted successfully!`);
+        } else {
+          toast.error(`Something went wrong!`);
+        }
+      })
+      .catch((err) => toast.error(`Something went wrong - ${err.message}`));
+  };
+
   return (
     <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
       <th
         scope="row"
-        className="px-6 py-4 w-50 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
       >
-        <img className="rounded-full" src={control.icon} alt="control" />
+        <img
+          className="rounded-full h-12 w-[5rem] object-cover"
+          src={control.topicAvatar}
+          alt="control"
+        />
       </th>
-      <td className="px-7 py-4">{control.name}</td>
-      <td className="px-3 py-4 text-center">{control.description}</td>
+      <td className="px-7 py-4">{control.topicName}</td>
+      <td className="px-3 py-4 text-center">{control.topicDesc}</td>
       <td className="px-6 py-4">
-        {/* {control.availability ? ( */}
-          <button
-            onClick={() => false}
-            disabled={true}
-            type="button"
-            className="px-3 py-1 text-xs text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-          >
-            Delete
-          </button>
-        {/* ) : ( */}
-          <button
-            onClick={() => false}
-            disabled={true}
-            type="button"
-            className="mt-2 px-3 py-1 text-xs text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-          >
-            Edit
-          </button>
-        {/* )} */}
+        <button
+          onClick={() => handleDelete()}
+          disabled={deleteInfo.isLoading}
+          type="button"
+          className="px-3 py-1 text-xs text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+        >
+          Delete
+        </button>
       </td>
     </tr>
   );
