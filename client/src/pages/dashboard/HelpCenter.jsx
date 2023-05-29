283812c8518ec42d85eabd0c13ba1deb65b8fd25
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-05-30 03:11:11 +0600

    modified:HelpCenter updated

--- client/src/pages/dashboard/HelpCenter.jsx
+++ client/src/pages/dashboard/HelpCenter.jsx
@@ -2,31 +2,52 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 import { Header } from "../../components";
 
-
-// import {
-//   useAddTopicContentMutation,
-//   useViewTopicQuery,
-// } from "../../services/apiSlice";
+import { useAddHelpsMutation } from "../../services/apiSlice";
 
 const initialData = {
-//
+  helpName: "",
+  helpAddress: "",
+  helpAvatar: "",
 };
 
 const HelpCenter = () => {
   const [formData, setFormData] = useState(initialData);
+  const [addHelps, responseInfo] = useAddHelpsMutation();
 
   const [fileAdded, setFileAdded] = useState(false);
 
   const handleFileChange = (event) => {
-  //  
+    if (event.target.files.length > 0) {
+      setFileAdded(true);
+    } else {
+      setFileAdded(false);
+    }
   };
 
   const handleSubmit = (e) => {
-    // 
+    e.preventDefault();
+    addHelps(formData)
+      .unwrap()
+      .then((res) => {
+        if (res.success) {
+          toast.success("New Volunteer added");
+          e.target.reset();
+          setFormData(initialData);
+        } else {
+          toast.error("Couldn't add the Volunteer");
+        }
+      })
+      .catch((e) => toast.error(e.message));
   };
 
   const handleOnChange = (e) => {
-// 
+    if (e.target.name === "topicAvatar") {
+      return setFormData((prev) => ({
+        ...prev,
+        helpAvatar: e.target.files[0],
+      }));
+    }
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   return (
@@ -50,11 +71,30 @@ const HelpCenter = () => {
               className="appearance-none block w-full bg-transparent border-sky-300 text-sky-300 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-transparent rounded-3xl"
               type="text"
               placeholder="name"
-              name="teacherName"
-              value="This will change by dynamic"
+              name="helpName"
+              value={formData.helpName}
             />
           </div>
           <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-sky-300	 text-xs font-bold mb-2 ml-5"
+              htmlFor="grid-first-name"
+            >
+              Address
+            </label>
+            <input
+              required
+              onChange={handleOnChange}
+              className="appearance-none block w-full bg-transparent border-sky-300 text-sky-300 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-transparent rounded-3xl"
+              type="text"
+              placeholder="address"
+              name="helpAddress"
+              value={formData.helpAddress}
+            />
+          </div>
+        </div>
+        <div>
+          <div className="w-full px-3">
             <div className="mb-4">
               <label
                 className="block uppercase tracking-wide text-sky-300	 text-xs font-bold mb-2 ml-5"
@@ -89,30 +129,6 @@ const HelpCenter = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              htmlFor="message"
-              className="block uppercase tracking-wide text-sky-300	 text-xs font-bold mb-2 ml-5"
-            >
-              Description
-            </label>
-            <textarea
-              required
-              id="message"
-              rows="4"
-              className="appearance-none block w-full bg-transparent border-sky-300 text-sky-300 border py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-transparent rounded-3xl"
-              type="message"
-              placeholder="Write details here..."
-              name="description"
-              value="This will change by dynamic"
-              onChange={handleOnChange}
-            />
-            <p className="text-sky-300 text-xs italic ml-5">
-              Make it as long and as crazy as you'd like
-            </p>
-          </div>
-        </div>
 
         <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
           <button className="Send-Button" type="submit">
@@ -125,4 +141,3 @@ const HelpCenter = () => {
 };
 
 export default HelpCenter;
-;
