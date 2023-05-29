6328cda126c85ffc389a70040815ad07857aa2f9
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-05-30 03:18:51 +0600

    modified:SingleTips updated

--- client/src/pages/tips/SingleTipPage.jsx
+++ client/src/pages/tips/SingleTipPage.jsx
@@ -1,9 +1,11 @@
 import { useLocation } from "react-router-dom";
 
 import poly1 from "../../assets/images/Polygon 1.svg";
+import poly2 from "../../assets/images/Polygon 1.svg";
+import poly3 from "../../assets/images/Polygon 1.svg";
 
 const SingleTipPage = () => {
-    const location = useLocation();
+  const location = useLocation();
 
   return (
     <div
@@ -23,7 +25,7 @@ const SingleTipPage = () => {
         style={{
           position: "absolute",
           top: "402px",
-          left: "198px",
+          left: "7%",
           backgroundColor: "#32363c",
           borderBottom: "2px solid #45d6f0",
           boxSizing: "border-box",
@@ -36,7 +38,7 @@ const SingleTipPage = () => {
           position: "absolute",
           top: "0px",
           left: "0px",
-          width: "1440px",
+          width: "100%",
           height: "356px",
           objectFit: "cover",
         }}
@@ -49,7 +51,7 @@ const SingleTipPage = () => {
           top: "0px",
           left: "0px",
           backgroundColor: "rgba(5, 5, 5, 0.3)",
-          width: "1440px",
+          width: "100%",
           height: "356px",
         }}
       />
@@ -58,17 +60,17 @@ const SingleTipPage = () => {
           position: "absolute",
           top: "-126.5px",
           left: "48.5px",
-          width: "1440px",
+          width: "100%",
           height: "97px",
         }}
         alt=""
-        src="/rectangle-45.svg"
+        src={poly2}
       />
       <div
         style={{
           position: "absolute",
           top: "178px",
-          left: "672px",
+          left: "50%",
           fontSize: "36px",
           fontWeight: "500",
         }}
@@ -78,32 +80,32 @@ const SingleTipPage = () => {
       <img
         style={{
           position: "absolute",
-          top: "427.66px",
-          left: "281.66px",
+          top: "42%",
+          left: "12%",
           borderRadius: "4px",
           width: "52.69px",
           height: "52.69px",
         }}
         alt=""
-        src="/polygon-1.svg"
+        src={poly3}
       />
       <img
         style={{
           position: "absolute",
           top: "583.66px",
-          left: "278.66px",
+          left: "12%",
           borderRadius: "4px",
           width: "52.69px",
           height: "52.69px",
         }}
         alt=""
-        src="/polygon-1.svg"
+        src={poly3}
       />
       <img
         style={{
           position: "absolute",
           top: "742.66px",
-          left: "278.66px",
+          left: "12%",
           borderRadius: "4px",
           width: "52.69px",
           height: "52.69px",
@@ -111,97 +113,41 @@ const SingleTipPage = () => {
         alt=""
         src={poly1}
       />
-      <b style={{ position: "absolute", top: "439px", left: "300px" }}>1</b>
-      <b style={{ position: "absolute", top: "595px", left: "296px" }}>2</b>
-      <b style={{ position: "absolute", top: "754px", left: "296px" }}>3</b>
+      <b style={{ position: "absolute", top: "439px", left: "13.5%" }}>1</b>
+      <b style={{ position: "absolute", top: "595px", left: "13.5%" }}>2</b>
+      <b style={{ position: "absolute", top: "754px", left: "13.5%" }}>3</b>
       <div
         style={{
           position: "absolute",
-          top: "431px",
-          left: "351px",
+          top: "42.5%",
+          left: "18%",
           fontSize: "22px",
           fontWeight: "600",
         }}
       >
-        {location.state.topicDesc}
+        {location.state.topicDescOne || "No description to show"}
       </div>
       <div
         style={{
           position: "absolute",
           top: "589px",
-          left: "351px",
+          left: "18%",
           fontSize: "22px",
           fontWeight: "600",
         }}
       >
-        Lorem ipsum dolor sit amet, consectetur
+        {location.state.topicDescTwo || "No description to show"}
       </div>
       <div
         style={{
           position: "absolute",
-          top: "747px",
-          left: "351px",
+          top: "746px",
+          left: "18%",
           fontSize: "22px",
           fontWeight: "600",
         }}
       >
-        Lorem ipsum dolor sit amet, consectetur
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          top: "466px",
-          left: "351px",
-          fontSize: "18px",
-          color: "#cccbcb",
-          textAlign: "justify",
-          display: "inline-block",
-          width: "819px",
-          height: "93px",
-        }}
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget
-        venenatis odio. Praesent et eros eros. Etiam fringilla est sed mi
-        efficitur pellentesque. Nunc tristique elit sapien. Fusce ut odio sit
-        amet odio feugiat consequat et et est.
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          top: "624px",
-          left: "351px",
-          fontSize: "18px",
-          color: "#cccbcb",
-          textAlign: "justify",
-          display: "inline-block",
-          width: "819px",
-          height: "93px",
-        }}
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget
-        venenatis odio. Praesent et eros eros. Etiam fringilla est sed mi
-        efficitur pellentesque. Nunc tristique elit sapien. Fusce ut odio sit
-        amet odio feugiat consequat et et est.
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          top: "782px",
-          left: "351px",
-          fontSize: "18px",
-          color: "#cccbcb",
-          textAlign: "justify",
-          display: "inline-block",
-          width: "819px",
-          height: "136px",
-        }}
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget
-        venenatis odio. Praesent et eros eros. Etiam fringilla est sed mi
-        efficitur pellentesque. Nunc tristique elit sapien. Fusce ut odio sit
-        amet odio feugiat consequat et et est.Duis vitae blandit lectus.
-        Vestibulum id ullamcorper augue. Cras non tincidunt quam, vel fringilla
-        elit. Ut porttitor sit amet mauris nec faucibus. Donec elementum
+        {location.state.topicDescThree || "No description to show"}
       </div>
     </div>
   );
