33fc36fc655304765d14a84258233e1ec7846147
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2023-05-30 03:15:43 +0600

    modified:HelpTableRow Updated for fetching data from backend

--- client/src/pages/helpCenter/HelpTableRow.jsx
+++ client/src/pages/helpCenter/HelpTableRow.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import userImg from "../../assets/images/Ellipse 17.svg"
 
 const HelpTableRow = ({help}) => {
+  
   return (
     <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
       <th
@@ -10,10 +11,10 @@ const HelpTableRow = ({help}) => {
         className="px-6 py-4 w-50 font-medium text-gray-900 whitespace-nowrap dark:text-white"
       >
         <div className="flex gap-2">
-          <img className="rounded-full" src={userImg} alt="help" />
+          <img className="rounded-full w-12 h-12" src={help?.helpAvatar} alt="help" />
           <div className="flex flex-col justify-center">
-            <span>{help.name}</span>
-            <span>{help.area}</span>
+            <span>{help?.helpName}</span>
+            <span>{help?.helpAddress}</span>
           </div>
         </div>
       </th>
